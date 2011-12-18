dc56cb17fdce77edf80afeae6fe422a47bd59147
jonathanliu2004@hotmail.com <jonathanliu2004@hotmail.com@43e4e6af-1bda-78f1-aefb-24612b55389e>
2011-12-18 10:53:08 +0000

    1 添加queryopen处理
    2 修改create流程，改进效率问题

--- trunk/crypto.c
+++ trunk/crypto.c
@@ -1,25 +1,5 @@
 #include "fileflt.h"
 
-PVOID
-SifsAllocateCryptContext(
-	VOID
-	)
-{
-	PCRYPT_CONTEXT cryptContext = NULL;
-
-	cryptContext = ExAllocatePoolWithTag(NonPagedPool, sizeof(*cryptContext), CRYPT_CONTEXT_TAG);
-
-	return cryptContext;
-}
-
-VOID
-SifsFreeCryptContext(
-	__in PVOID CryptContext
-	)
-{
-	ExFreePoolWithTag(CryptContext, CRYPT_CONTEXT_TAG);
-}
-
 VOID
 SifsInitializeCryptContext(
 	__inout PCRYPT_CONTEXT CryptContext
